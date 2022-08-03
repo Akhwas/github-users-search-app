a3df60a8dd133e2e4fc5853fc0a7c85ad9247263
38807750 <38807750+Akhwas@users.noreply.github.com>
2022-08-04 03:16:22 +1000

    authorizing login/logout setup

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
+// domain: 
+// clientID: 
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+        const domain= {process.env.REACT_APP_DOMAIN}
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        redirectUri={window.location.origin}>      
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
